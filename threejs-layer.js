4454f163dda195a14aa927271176265ec3355235
Felipe Figueroa <amenadiel@gmail.com>
2014-10-13 16:20:44 -0300

    allows thdds canvas rendering logic

--- threejs-layer.js
+++ threejs-layer.js
@@ -24,16 +24,16 @@
  * @param {Object}   options  Options passed to initialize method.
  * @param {Function} callback Callback to execute when map was updated.
  */
-function ThreejsLayer(options, callback){
-  this.bindAll();
-  this.callback = callback;
-  this.initialize(options || {});
+function ThreejsLayer(options, callback) {
+	this.bindAll();
+	this.callback = callback;
+	this.initialize(options || {});
 
-  this.firstRun = true;
+	this.firstRun = true;
 
-  if (options.map) {
-    this.setMap(options.map);
-  }
+	if (options.map) {
+		this.setMap(options.map);
+	}
 }
 
 /**
@@ -45,215 +45,256 @@ ThreejsLayer.prototype = new google.maps.OverlayView();
 
 /**
  * Get browser specifiv CSS transform property.
- * 
+ *
  * @return {String} The property.
  */
-ThreejsLayer.CSS_TRANSFORM = (function() {
-  var div = document.createElement('div');
-  var props = [
-    'transform',
-    'WebkitTransform',
-    'MozTransform',
-    'OTransform',
-    'msTransform'
-  ];
+ThreejsLayer.CSS_TRANSFORM = (function () {
+	var div = document.createElement('div');
+	var props = [
+		'transform',
+		'WebkitTransform',
+		'MozTransform',
+		'OTransform',
+		'msTransform'
+	];
 
-  for (var i = 0; i < props.length; i++) {
-    var prop = props[i];
-    if (div.style[prop] !== undefined) {
-      return prop;
-    }
-  }
+	for (var i = 0; i < props.length; i++) {
+		var prop = props[i];
+		if (div.style[prop] !== undefined) {
+			return prop;
+		}
+	}
 
-  return props[0];
+	return props[0];
 })();
 
 /**
  * Bind all methods to the instance.
  */
-ThreejsLayer.prototype.bindAll = function(){
-  var instance = this;
+ThreejsLayer.prototype.bindAll = function () {
+	var instance = this;
 
-  function bind(name) {
-    var method = instance[name];
-    if (typeof method != "function"){ return; }
-    instance[name] = function() { return method.apply(instance, arguments); };
-  }
+	function bind(name) {
+		var method = instance[name];
+		if (typeof method != "function") {
+			return;
+		}
+		instance[name] = function () {
+			return method.apply(instance, arguments);
+		};
+	}
 
-  for (var all in instance){ bind(all); }
+	for (var all in instance) {
+		bind(all);
+	}
 };
 
 /**
  * Initialize the layer with the given options.
  * @param  {Object} options - Options
  */
-ThreejsLayer.prototype.initialize = function(options){
+ThreejsLayer.prototype.initialize = function (options) {
 
-  this.options = options;
+	this.options = options;
 
-  this.camera = new THREE.OrthographicCamera(0, 255, 0, 255, -3000, 3000);
-  this.camera.position.z = 1000;
+	this.camera = new THREE.OrthographicCamera(0, 255, 0, 255, -3000, 3000);
+	this.camera.position.z = 1000;
+	this.renderertype = options.renderertype || '';
+	this.scene = new THREE.Scene();
 
-  this.scene = new THREE.Scene();
+	this.webgl = (function () {
+		try {
+			var canvas = document.createElement('canvas');
+			return !!window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
+		} catch (e) {
+			return false;
+		}
+	})();
 
-  this.renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    clearColor: 0x000000,
-    clearAlpha: 0
-  });
 
-  this.canvas = this.renderer.domElement;
+	if (this.renderertype == 'Canvas' || !this.webgl) {
+		this.renderer = new THREE.CanvasRenderer({
+			alpha: true,
+			clearColor: 0x000000,
+			clearAlpha: 0
+		});
+		this.renderertype = 'Canvas';
+	} else {
+		this.renderer = new THREE.WebGLRenderer({
+			alpha: true,
+			clearColor: 0x000000,
+			clearAlpha: 0
+		});
+		this.renderertype = 'WebGL';
+	}
+
+	this.canvas = this.renderer.domElement;
 };
 
 /**
- * This method is called once after setMap() is called with a valid map. 
+ * This method is called once after setMap() is called with a valid map.
  * @see https://developers.google.com/maps/documentation/javascript/reference#OverlayView
  */
-ThreejsLayer.prototype.onAdd = function() {
+ThreejsLayer.prototype.onAdd = function () {
 
-  this.map = this.getMap();
+	this.map = this.getMap();
 
-  this.getPanes().overlayLayer.appendChild(this.canvas);
+	this.getPanes().overlayLayer.appendChild(this.canvas);
 
-  this.changeHandler = google.maps.event.addListener(
-    this.map,
-    'bounds_changed',
-    this.draw
-  );
+	this.changeHandler = google.maps.event.addListener(
+		this.map,
+		'bounds_changed',
+		this.draw
+	);
 
-  this.draw();
+	this.draw();
 };
 
 /**
  * This method is called once following a call to setMap(null).
  * @see https://developers.google.com/maps/documentation/javascript/reference#OverlayView
  */
-ThreejsLayer.prototype.onRemove = function() {
+ThreejsLayer.prototype.onRemove = function () {
 
-  if (!this.map) { return; }
+	if (!this.map) {
+		return;
+	}
 
-  this.map = null;
+	this.map = null;
 
-  this.canvas.parentElement.removeChild(this.canvas);
+	this.canvas.parentElement.removeChild(this.canvas);
 
-  if (this.changeHandler) {
-    google.maps.event.removeListener(this.changeHandler);
-    this.changeHandler = null;
-  }
+	if (this.changeHandler) {
+		google.maps.event.removeListener(this.changeHandler);
+		this.changeHandler = null;
+	}
 };
 
 /**
  * This method is called when the layer postion needs an update.
  */
-ThreejsLayer.prototype.draw = function() {
+ThreejsLayer.prototype.draw = function () {
 
-  if (!this.map) { return; }
+	if (!this.map) {
+		return;
+	}
 
-  var bounds = this.map.getBounds();
+	var bounds = this.map.getBounds();
 
-  var topLeft = new google.maps.LatLng(
-    bounds.getNorthEast().lat(),
-    bounds.getSouthWest().lng()
-  );
+	var topLeft = new google.maps.LatLng(
+		bounds.getNorthEast().lat(),
+		bounds.getSouthWest().lng()
+	);
 
-  var projection = this.getProjection();
-  var point = projection.fromLatLngToDivPixel(topLeft);
+	var projection = this.getProjection();
+	var point = projection.fromLatLngToDivPixel(topLeft);
 
-  this.canvas.style[ThreejsLayer.CSS_TRANSFORM] = 'translate(' +
-      Math.round(point.x) + 'px,' +
-      Math.round(point.y) + 'px)';
+	this.canvas.style[ThreejsLayer.CSS_TRANSFORM] = 'translate(' +
+		Math.round(point.x) + 'px,' +
+		Math.round(point.y) + 'px)';
 
-  if (this.firstRun) {
-    this.firstRun = false;
-    
-    if (this.callback){
-      this.callback(this);
-    }
-  }
+	if (this.firstRun) {
+		this.firstRun = false;
 
-  this.update();
+		if (this.callback) {
+			this.callback(this);
+		}
+	}
+
+	this.update();
 };
 
 /**
  * Call this method when the layer's size changed.
  */
-ThreejsLayer.prototype.resize = function(){
+ThreejsLayer.prototype.resize = function () {
 
-  if (!this.map){ return; }
+	if (!this.map) {
+		return;
+	}
 
-  var div = this.map.getDiv(),
-    width = div.clientWidth,
-    height = div.clientHeight;
+	var div = this.map.getDiv(),
+		width = div.clientWidth,
+		height = div.clientHeight;
 
-  if (width == this.width && height == this.height){ return; }
+	if (width == this.width && height == this.height) {
+		return;
+	}
 
-  this.width = width;
-  this.height = height;
+	this.width = width;
+	this.height = height;
 
-  this.renderer.setSize(width, height);
-  this.update();
+	this.renderer.setSize(width, height);
+	this.update();
 };
 
 /**
  * This method is called when the Three.js camera needs an update.
  */
-ThreejsLayer.prototype.update = function() {
+ThreejsLayer.prototype.update = function () {
 
-  var projection = this.map.getProjection(),
-    zoom, scale, offset, bounds, topLeft;
+	var projection = this.map.getProjection(),
+		zoom, scale, offset, bounds, topLeft;
 
-  if (!projection){ return; }
+	if (!projection) {
+		return;
+	}
 
-  bounds = this.map.getBounds();
+	bounds = this.map.getBounds();
 
-  topLeft = new google.maps.LatLng(
-    bounds.getNorthEast().lat(),
-    bounds.getSouthWest().lng()
-  );
+	topLeft = new google.maps.LatLng(
+		bounds.getNorthEast().lat(),
+		bounds.getSouthWest().lng()
+	);
 
-  zoom = this.map.getZoom();
-  scale = Math.pow(2, zoom);
-  offset = projection.fromLatLngToPoint(topLeft);
+	zoom = this.map.getZoom();
+	scale = Math.pow(2, zoom);
+	offset = projection.fromLatLngToPoint(topLeft);
 
-  this.resize();
+	this.resize();
 
-  this.camera.position.x = offset.x;
-  this.camera.position.y = offset.y;
+	this.camera.position.x = offset.x;
+	this.camera.position.y = offset.y;
 
-  this.camera.scale.x = this.width / 256 / scale;
-  this.camera.scale.y = this.height / 256 / scale;
+	this.camera.scale.x = this.width / 256 / scale;
+	this.camera.scale.y = this.height / 256 / scale;
 
-  this.render();
+	if (this.renderertype == 'Canvas') {
+		//console.log(this.scene,this.camera);
+		this.renderer.render(this.scene, this.camera);
+	} else {
+		this.render();
+	}
 };
 
 /**
  * Renders the layer deferred.
  */
-ThreejsLayer.prototype.render = function() {
-  cancelAnimationFrame(this.animationFrame);
-  this.animationFrame = requestAnimationFrame(this.deferredRender);
+ThreejsLayer.prototype.render = function () {
+	cancelAnimationFrame(this.animationFrame);
+	this.animationFrame = requestAnimationFrame(this.deferredRender);
 };
 
 /**
  * The final rendering. If you have passed a function to `options.render`
  * it will be executed here.
  */
-ThreejsLayer.prototype.deferredRender = function(){
-  if (typeof this.options.render === false) {
-    return;
-  } else if (typeof this.options.render == "function"){
-    this.options.render();
-  } else {
-    this.renderer.render( this.scene, this.camera );
-  }
+ThreejsLayer.prototype.deferredRender = function () {
+	if (typeof this.options.render === false) {
+		return;
+	} else if (typeof this.options.render == "function") {
+		this.options.render();
+	} else {
+		this.renderer.render(this.scene, this.camera);
+	}
 };
 
 /**
  * Shortcut method to add new geometry to the scene.
  * @param  {Geometry} geometry The Three.js geometry to add.
  */
-ThreejsLayer.prototype.add = function(geometry){
-  this.scene.add(geometry);
+ThreejsLayer.prototype.add = function (geometry) {
+	this.scene.add(geometry);
 };
 
 /**
@@ -261,14 +302,14 @@ ThreejsLayer.prototype.add = function(geometry){
  * @param  {google.maps.LatLng} latLng - The LatLng to convert.
  * @return {THREE.Vector3} The resulting vertex.
  */
-ThreejsLayer.prototype.fromLatLngToVertex = function(latLng) {
-  var projection = this.map.getProjection(),
-    point = projection.fromLatLngToPoint(latLng),
-    vertex = new THREE.Vector3();
+ThreejsLayer.prototype.fromLatLngToVertex = function (latLng) {
+	var projection = this.map.getProjection(),
+		point = projection.fromLatLngToPoint(latLng),
+		vertex = new THREE.Vector3();
 
-  vertex.x = point.x;
-  vertex.y = point.y;
-  vertex.z = 0;
+	vertex.x = point.x;
+	vertex.y = point.y;
+	vertex.z = 0;
 
-  return vertex;
+	return vertex;
 };
